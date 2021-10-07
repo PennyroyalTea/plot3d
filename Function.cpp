6de34cbbfdce03765e4bb3af43a8fbf03c7991f7
Boris Starkov <boris.starkow@gmail.com>
2021-10-07 17:06:54 +0300

    std move Function constructor params

--- Function.cpp
+++ Function.cpp
@@ -1,9 +1,15 @@
 #include "Function.h"
 
+#include <utility>
+
 Function::Function(std::function<float(float, float, float)> f,
                    std::pair<float, float> xRange,
                    std::pair<float, float> yRange,
-                   std::pair<float, float> zRange) : f(f), xRange(xRange), yRange(yRange), zRange(zRange) {}
+                   std::pair<float, float> zRange) :
+                    f(std::move(f)),
+                    xRange(std::move(xRange)),
+                    yRange(std::move(yRange)),
+                    zRange(std::move(zRange)) {}
 
 float Function::operator()(float x, float y, float t) const {
     return f(x, y, t);
