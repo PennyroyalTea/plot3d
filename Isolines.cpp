0651fae9fb6ce0fe4ee452d35cc11282ca643445
Boris Starkov <boris.starkow@gmail.com>
2021-10-12 19:13:00 +0300

    use ebo to draw isolines

--- Isolines.cpp
+++ Isolines.cpp
@@ -1,6 +1,15 @@
 #include "Isolines.h"
 
-void addMarchingTriangle(std::vector<vertex>& res, const Function& f, int i[3], int j[3], float stepX, float stepY, float z, float t) {
+void addMarchingTriangle(
+        std::vector<vertex>& vertices,
+        std::vector<uint32_t>& order,
+        std::map<std::pair<std::pair<int, int>, int>, int>& id_map, // {edge, isolineId} -> orderId
+        int isolineId,
+        const Function& f,
+        int i[3], int j[3], int id[3],
+        float stepX, float stepY,
+        float z, float t) {
+
     float x[3];
     float y[3];
     float fx[3];
@@ -11,23 +20,38 @@ void addMarchingTriangle(std::vector<vertex>& res, const Function& f, int i[3], 
     }
     for (int iter = 0; iter < 3; ++iter) {
         int iter1 = (iter + 1) % 3;
-        if ((fx[iter] - z) * (fx[iter1] - z) < 0) {
+        if ((fx[iter] - z) * (fx[iter1] - z) > 0) {
+            continue;
+        }
+
+        std::pair<int, int> edge = {id[iter], id[iter1]};
+        std::pair<int, int> edge_back = {id[iter1], id[iter]};
+
+        if (id_map.count({edge, isolineId})) {
+            order.push_back(id_map[{edge, isolineId}]);
+        } else if (id_map.count({edge_back, isolineId})) {
+            order.push_back(id_map[{edge_back, isolineId}]);
+        } else {
+            id_map[{edge, isolineId}] = vertices.size();
             float xNorm1 = 2 * ((x[iter1] - f.xRange.first) / (f.xRange.second - f.xRange.first)) - 1.f;
             float xNorm = 2 * ((x[iter] - f.xRange.first) / (f.xRange.second - f.xRange.first)) - 1.f;
             float yNorm1 = 2 * ((y[iter1] - f.yRange.first) / (f.yRange.second - f.yRange.first)) - 1.f;
             float yNorm = 2 * ((y[iter] - f.yRange.first) / (f.yRange.second - f.yRange.first)) - 1.f;
             float zNorm = 2 * ((z - f.zRange.first) / (f.zRange.second - f.zRange.first)) - 1.f;
             float k = abs(fx[iter] - z) / abs(fx[iter] - fx[iter1]);
-            res.push_back(
+            vertices.push_back(
                     {k * xNorm1 + (1 - k) * xNorm, k * yNorm1 + (1 - k) * yNorm, zNorm,
                      {0, 255, 0}});
+            order.push_back(id_map[{edge, isolineId}]);
         }
     }
 
 }
 
-std::vector<vertex> generateIsolinesVertices(int gridN, int isolinesN, Function f, float t) {
-    std::vector<vertex> res;
+std::pair<std::vector<vertex>, std::vector<uint32_t>> generateIsolines(int gridN, int isolinesN, Function f, float t) {
+    std::vector<vertex> vertices_res;
+    std::vector<uint32_t> order_res;
+    std::map<std::pair<std::pair<int, int>, int>, int> id_map;
 
     float stepX = (f.xRange.second - f.xRange.first) / gridN;
     float stepY = (f.yRange.second - f.yRange.first) / gridN;
@@ -43,19 +67,26 @@ std::vector<vertex> generateIsolinesVertices(int gridN, int isolinesN, Function 
                 int j1[3] = {j, j + 1, j};
                 int i2[3] = {i, i + 1, i + 1};
                 int j2[3] = {j + 1, j, j + 1};
-                addMarchingTriangle(res, f, i1, j1, stepX, stepY, z, t);
-                addMarchingTriangle(res, f, i2, j2, stepX, stepY, z, t);
+                int id1[3] = {i * gridN + j, i * gridN + j + 1, (i + 1) * gridN + j};
+                int id2[3] = {i * gridN + j + 1, (i + 1) * gridN + j, (i + 1) * gridN + (j + 1)};
+                addMarchingTriangle(vertices_res, order_res, id_map, isolineId, f, i1, j1, id1, stepX, stepY, z, t);
+                addMarchingTriangle(vertices_res, order_res, id_map, isolineId, f, i2, j2, id2, stepX, stepY, z, t);
             }
         }
     }
-    return res;
+    return {vertices_res, order_res};
 }
 
 Isolines::Isolines(Function f, int gridN, int isolinesN) : f(std::move(f)), gridN(gridN), isolinesN(isolinesN) {
-    vertices = generateIsolinesVertices(gridN, isolinesN, f, 0);
+    auto [vertices, order] = generateIsolines(gridN, isolinesN, f, 0);
 
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, order.size() * sizeof(std::uint32_t), order.data(), GL_DYNAMIC_DRAW);
+
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -70,12 +101,16 @@ Isolines::Isolines(Function f, int gridN, int isolinesN) : f(std::move(f)), grid
 }
 
 void Isolines::draw(float t) {
-    vertices = generateIsolinesVertices(gridN, isolinesN, f, t);
+    auto [vertices, order] = generateIsolines(gridN, isolinesN, f, t);
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertex), vertices.data(), GL_STREAM_DRAW);
-    glDrawArrays(GL_LINES, 0, vertices.size());
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, order.size() * sizeof(std::uint32_t), order.data(), GL_DYNAMIC_DRAW);
+
+    glDrawElements(GL_LINES, order.size(), GL_UNSIGNED_INT, nullptr);
 }
 
 void Isolines::updateSettings(const std::map<int, int>& settings) {
